d8011f3198c171a0a2306dbd9538a8282546bbd1
Ahmed Elshaabany <ahmed@savedroid.de>
2020-06-19 01:41:30 +0200

    Update ios & android folders

--- template/index.js
+++ template/index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
+import {name as appName} from './app.json';
 
-const APP_NAME = 'ProjectName';
-
-AppRegistry.registerComponent(APP_NAME, () => App);
+AppRegistry.registerComponent(appName, () => App);
