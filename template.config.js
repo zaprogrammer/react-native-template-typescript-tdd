13ab429dce6da0ae29ed394631341807168ad61d
Ahmed Elshaabany <ahmed@savedroid.de>
2020-06-19 01:30:18 +0200

    Modify package.json file

--- template.config.js
+++ template.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
-    placeholderName: 'ProjectName',
+    placeholderName: 'HelloWorld',
   
     // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
     // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
@@ -10,5 +10,5 @@ module.exports = {
     templateDir: './template',
   
     // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
-    postInitScript: './script.js',
+    // postInitScript: './script.js',
   };
